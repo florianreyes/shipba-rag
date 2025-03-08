83ffcc1833d8fb58c93da082af65039a8909afc0
florianreyes <florianreyes541@gmail.com>
2025-03-08 16:31:21 -0300

    DB schema update

---
+++ lib/db/migrations/0000_absent_the_phantom.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS "embeddings" (
+	"id" varchar(191) PRIMARY KEY NOT NULL,
+	"user_id" varchar(191),
+	"content" text NOT NULL,
+	"embedding" vector(1536) NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" varchar(191) PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"mail" text NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "workspaces" (
+	"id" varchar(191) PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "workspaces_users" (
+	"id" varchar(191) PRIMARY KEY NOT NULL,
+	"workspace_id" varchar(191) NOT NULL,
+	"user_id" varchar(191) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "embeddings" ADD CONSTRAINT "embeddings_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "workspaces_users" ADD CONSTRAINT "workspaces_users_workspace_id_workspaces_id_fk" FOREIGN KEY ("workspace_id") REFERENCES "public"."workspaces"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "embeddingIndex" ON "embeddings" USING hnsw ("embedding" vector_cosine_ops);
--- lib/db/schema/workspaces_users.ts
+++ lib/db/schema/workspaces_users.ts
@@ -5,6 +5,7 @@ import { z } from "zod";
 
 import { nanoid } from "@/lib/utils";
 import { workspaces } from "./workspaces";
+import { users } from "./users";
 
 export const workspacesUsers = pgTable("workspaces_users", {
   id: varchar("id", { length: 191 })
@@ -13,7 +14,9 @@ export const workspacesUsers = pgTable("workspaces_users", {
   workspaceId: varchar("workspace_id", { length: 191 })
     .notNull()
     .references(() => workspaces.id),
-  userId: varchar("user_id", { length: 191 }).notNull(),
+  userId: varchar("user_id", { length: 191 }).notNull().references(
+    () => users.id,
+    { onDelete: "cascade" },),
   createdAt: timestamp("created_at")
     .notNull()
     .default(sql`now()`)
